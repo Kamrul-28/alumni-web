e49a933399af1f254d8f257ace7799ea7d1f2ba6
SaimumIslam <saimumislam27@gmail.com>
2024-03-25 23:55:43 +0600

    empty object in query

--- client/src/utils/check.js
+++ client/src/utils/check.js
@@ -1,3 +1,7 @@
 export function isObject(obj) {
   return obj != null && obj.constructor.name === "Object";
 }
+
+export function isEmptyObj(obj) {
+  return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
